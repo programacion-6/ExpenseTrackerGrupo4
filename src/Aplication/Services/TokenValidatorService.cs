d071fa287adca0deba0e4e6be9ccbab208824da1
fabian-romero-jalafund <fabian.romero@fundacion-jala.org>
2024-09-20 04:16:38 -0400

    chore: move token key to constants

--- src/Aplication/Services/TokenValidatorService.cs
+++ src/Aplication/Services/TokenValidatorService.cs
@@ -3,18 +3,17 @@ using System.Security.Claims;
 using System.Text;
 using ExpenseTrackerGrupo4.src.Aplication.Interfaces;
 using ExpenseTrackerGrupo4.src.Domain.Entities;
+using ExpenseTrackerGrupo4.src.Utils;
 using Microsoft.IdentityModel.Tokens;
 
 namespace ExpenseTrackerGrupo4.src.Aplication.Services;
 
 public class TokenValidatorService() : ITokenValidatorService
 {
-    private readonly string _secretKey = "a5837a1eb030d1e5b6fd4d77b45c227c";
-
     public string GenerateToken(User user)
     {
         var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_secretKey);
+        var key = Encoding.ASCII.GetBytes(TokenValidatorConstants._secretKey);
 
         var tokenDescriptor = new SecurityTokenDescriptor
         {
@@ -24,7 +23,7 @@ public class TokenValidatorService() : ITokenValidatorService
                     new Claim(ClaimTypes.Email, user.Email)
                 ]
             ),
-            Expires = DateTime.UtcNow.AddHours(1),
+            Expires = DateTime.UtcNow.AddHours(2),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),
                 SecurityAlgorithms.HmacSha256Signature
@@ -37,57 +36,6 @@ public class TokenValidatorService() : ITokenValidatorService
 
     public bool ValidateToken(string token)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_secretKey);
-
-        try
-        {
-            tokenHandler.ValidateToken(
-                token,
-                new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ClockSkew = TimeSpan.Zero
-                },
-                out SecurityToken validatedToken
-            );
-
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
-    public ClaimsPrincipal? GetClaimsFromToken(string token)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_secretKey);
-
-        try
-        {
-            var claimsPrincipal = tokenHandler.ValidateToken(
-                token,
-                new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ClockSkew = TimeSpan.Zero
-                },
-                out SecurityToken validatedToken
-            );
-
-            return claimsPrincipal;
-        }
-        catch
-        {
-            return null;
-        }
+        return false;
     }
 }
---
+++ src/Utils/TokenValidatorConstants.cs
@@ -0,0 +1,6 @@
+namespace ExpenseTrackerGrupo4.src.Utils;
+
+public class TokenValidatorConstants
+{
+    public const string _secretKey = "a5837a1eb030d1e5b6fd4d77b45c227c";
+}
