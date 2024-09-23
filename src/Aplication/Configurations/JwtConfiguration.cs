b61a16de60677ab9b025e2e5fbf5fa1550ebad13
LuchoVera1 <luis.espinoza.13875568@usalesiana.edu.bo>
2024-09-22 22:12:37 -0400

    feat: added user login and authorize

---
+++ src/Aplication/Configurations/JwtConfiguration.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ExpenseTrackerGrupo4.src.Utils;
+
+namespace ExpenseTrackerGrupo4.Configurations
+
+{
+    public static class JwtConfiguration
+    {
+        public static void AddJwtAuthentication(this IServiceCollection services)
+        {
+            var key = Encoding.ASCII.GetBytes(TokenValidatorConstants._secretKey);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
--- src/Aplication/Interfaces/IAuthenticationService.cs
+++ src/Aplication/Interfaces/IAuthenticationService.cs
@@ -5,4 +5,5 @@ namespace ExpenseTrackerGrupo4.src.Aplication.Interfaces;
 public interface IAuthenticationService
 {
     Task<string> RegisterAsync(User user);
+    Task<string> LoginAsync(string email, string password);
 }
