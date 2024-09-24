97ef6fab92f0460fb52731850f85f60f725e308c
santiago-caballero-jalafund <santicm95386@gmail.com>
2024-09-24 00:37:52 -0400

    feat: income integrated to the program

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,21 +1,20 @@
 using ExpenseTrackerGrupo4.src.Domain.Contexts;
 using System.Data;
-using ExpenseTrackerGrupo4.src.Aplication.Interfaces;
-using ExpenseTrackerGrupo4.src.Aplication.Services;
 using ExpenseTrackerGrupo4.src.Infrastructure.Interfaces;
 using ExpenseTrackerGrupo4.src.Infrastructure.Repositories;
 using Npgsql;
 using ExpenseTrackerGrupo4.src.Presentation.Profiles;
-using ExpenseTrackerGrupo4.src.Aplication.Commands;
 using DotNetEnv;
 using ExpenseTrackerGrupo4.Configurations;
+using ExpenseTrackerGrupo4.src.Aplication.Interfaces;
+using ExpenseTrackerGrupo4.src.Aplication.Services;
+using ExpenseTrackerGrupo4.src.Aplication.Commands;
+using ExpenseTrackerGrupo4.src.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 Env.Load();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddJwtAuthentication();
 builder.Services.AddSwaggerWithJwt();
 builder.Services.AddControllers();
@@ -23,6 +22,10 @@ builder.Services.AddScoped<CommandInvoker>();
 builder.Services.AddScoped<ITokenValidatorService, TokenValidatorService>();
 builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddScoped<IIncomeRepository, IncomeRepository>();
+builder.Services.AddScoped<IIncomeService, IncomeService>();
+
 builder.Services.AddAutoMapper(typeof(ExpenseTrackerProfile));
 
 builder.Services.AddTransient<IDbConnection>(sp => 
@@ -32,14 +35,13 @@ builder.Services.AddScoped<BaseContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => 
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Expense Tracker API v1");
-        c.RoutePrefix = string.Empty; // Establece la interfaz de Swagger en la raíz
+        c.RoutePrefix = string.Empty;
     });
 }
 
