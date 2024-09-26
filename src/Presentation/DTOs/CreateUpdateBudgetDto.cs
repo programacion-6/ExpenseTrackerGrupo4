3c26e45dc93ad8035eda7395c827f5e7c431d0d6
LuchoVera1 <luis.espinoza.13875568@usalesiana.edu.bo>
2024-09-26 01:39:33 -0400

    feat: add budget controllers

---
+++ src/Presentation/DTOs/CreateUpdateBudgetDto.cs
@@ -0,0 +1,7 @@
+namespace ExpenseTrackerGrupo4.src.Presentation.DTOs;
+
+public class CreateUpdateBudgetDto
+{
+    public DateTime Month { get; set; }
+    public decimal BudgetAmount { get; set; }
+}
