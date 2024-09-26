45aaa4b0eefff9430b3414b164c9ebffe5138c52
LuchoVera1 <luis.espinoza.13875568@usalesiana.edu.bo>
2024-09-26 01:38:27 -0400

    chore: add new model and fix budget model

--- src/Domain/Entities/Budget.cs
+++ src/Domain/Entities/Budget.cs
@@ -5,7 +5,7 @@ namespace ExpenseTrackerGrupo4.src.Domain.Entities;
 public class Budget : IEntity
 {
     public Guid Id { get; init; } = Guid.NewGuid();
-    public required Guid UserId { get; init; }
+    public required Guid UserId { get; set; }
     public required DateTime Month { get; set; }
     public required decimal BudgetAmount { get; set; }
 }
